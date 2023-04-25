d37981f7e8466ce8609abe5f47da41aec2805b9f
Thecodingyogi <cynthiatonui@gmail.com>
2023-04-25 09:22:37 -0700

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,28 +1,46 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * @list: Integers to be sorted
+ * swap - swap two nodes
+ * @head: pointer to the head
+ * @node1: pointer to the first node
+ * @node2: The second node
+ */
+
+void swap(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list using insertion sort
+ * @list: A pointer to the head of a doubly linked list
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i = *list;
-	listint_t *j, *temp;
+	listint_t *node, *current, *temp;
 
-	if (list == NULL || i == NULL || i->next == NULL)
+	if (!list || !*list)
 		return;
-	for (i = *list; i->next != NULL; i = i->next)
+	for (node = (*list)->next; node != NULL; node = temp)
 	{
-		for (j = i->next; j != NULL; j = j->next)
+		temp = node->next;
+		current = node->prev;
+		while (current != NULL && node->n < current->n)
 		{
-			if (i->n > j->n)
-			{
-				temp = j->n;
-				j->n = i->n;
-				i->n = temp;
-				print_list(*list);
-			}
+			swap(list, &current, node);
+			print_list((const listint_t *)*list);
 		}
 	}
 }
